2d582216668fda6278973878e57ba2bd97fec200
Rolltmp <tmpgaming080@gmail.com>
2020-03-12 22:26:16 +0200

    init

--- src/main/java/servlet/About.java
+++ src/main/java/servlet/About.java
@@ -14,7 +14,7 @@ public class About extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        // this is just a comment to see hoe the branches work
         RequestDispatcher dispatcher = req.getRequestDispatcher("page/about.jsp");
         dispatcher.forward(req,resp);
         ServletVariables.previousPath = req.getServletPath();
